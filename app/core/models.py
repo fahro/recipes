54d0ab34895867f565753d660d81f146c4953643
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-04-29 18:26:01 +0200

    Create user API

--- app/core/models.py
+++ app/core/models.py
@@ -10,18 +10,20 @@ class UserManager(BaseUserManager):
 
         if not email:
             raise ValueError("Users must have an email address")
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+
+        user = self.model(
+            email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new superuser"""
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(self._db)
+        user.save(using=self._db)
 
         return user
 
